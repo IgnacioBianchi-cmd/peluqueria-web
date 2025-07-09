caf545a418fc1b1db56c4345c0da87cccc8d0747
JoseTH00 <joseoscarthorlet@gmail.com>
2025-07-09 19:27:23 -0300

    QR en el historial de turnos agregado

--- mvc-app/Pages/HistorialTurnos.cshtml.cs
+++ mvc-app/Pages/HistorialTurnos.cshtml.cs
@@ -4,7 +4,12 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using mvc_app.Data;
+using QRCoder;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Threading.Tasks;
 using TurneroApp.Models;
 
@@ -22,7 +27,13 @@ namespace mvc_app.Pages
             _userManager = userManager;
         }
 
-        public List<Turno> Turnos { get; set; } = new();
+        public List<TurnoConQR> Turnos { get; set; } = new();
+
+        public class TurnoConQR
+        {
+            public Turno Turno { get; set; }
+            public string QrBase64 { get; set; } = "";
+        }
 
         public async Task<IActionResult> OnGetAsync()
         {
@@ -30,14 +41,31 @@ namespace mvc_app.Pages
             if (usuario == null)
                 return RedirectToPage("/Account/Login");
 
-            Turnos = await _context.Turnos
+            var turnos = await _context.Turnos
                 .Include(t => t.TurnosServicios)
                     .ThenInclude(ts => ts.Servicio)
                 .Where(t => t.UsuarioId == usuario.Id)
                 .OrderByDescending(t => t.FechaHora)
                 .ToListAsync();
 
+            foreach (var t in turnos)
+            {
+                var qrImage = GenerarQrBase64(t.QrToken);
+                Turnos.Add(new TurnoConQR { Turno = t, QrBase64 = qrImage });
+            }
+
             return Page();
+        }
+
+        private string GenerarQrBase64(string contenido)
+        {
+            using var qrGenerator = new QRCodeGenerator();
+            using var qrData = qrGenerator.CreateQrCode(contenido, QRCodeGenerator.ECCLevel.Q);
+            using var qrCode = new QRCode(qrData);
+            using var bitmap = qrCode.GetGraphic(20);
+            using var ms = new MemoryStream();
+            bitmap.Save(ms, ImageFormat.Png);
+            return "data:image/png;base64," + Convert.ToBase64String(ms.ToArray());
         }
     }
 }
